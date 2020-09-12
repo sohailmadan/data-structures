b65cd12a410dc2acf100fffc01225891a94bcaf5
sohailmadan <48715594+sohailmadan@users.noreply.github.com>
2020-09-12 13:13:49 +0530

    Update quick.java

--- quick.java
+++ quick.java
@@ -1,39 +1,35 @@
-public class quick {
-    public static void quicksort(int[]a,int start,int end){
-        if(start<end){
-            int pivotindex=partition(a,start,end);
-            quicksort(a,start,pivotindex-1);
-            quicksort(a,pivotindex+1,end);
-        }
+import java.util.Arrays;
+public class Quick{
+    public void quicksort(int[]arr,int start,int end){
+         if(start<end){
+            int pivotindex = partition(arr,start,end);
+           
+            quicksort(arr,start,pivotindex-1);
+            quicksort(arr,pivotindex+1,end);
+         }
     }
-    public static int partition(int[]a,int start,int end){
-        int pivot=a[end];
-        int i =start;
-        for(int j=start;j<=end-1;j++){
-            if(a[j]<pivot){
-                int temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
+    public static int partition(int []arr,int start , int end){
+        int pivot = arr[end];
+        int i = start;
+        int temp=0;
+        for(int j = start; j<=end-1;j++){
+            if(arr[j] < pivot){
+                temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
                 i++;
             }
-                    }
-        a[end] =a[i];
-        a[i] =pivot;
-        return i;
-        
-    }
-    public static void printarray(int[]a){
-        for(int i=0;i<a.length;i++){
-            System.out.print(" "+a[i]);
-            
         }
-        
+        arr[end]=arr[i];
+        arr[i]=pivot;
+        return i;
     }
+    
     public static void main(String[]args){
-        int[]a={5,9,6,7,2,48,96,75};
-        quicksort(a,0,a.length-1);
-         printarray(a);
+        Quick q = new Quick();
+        int []arr={1,45,67,23,43,98,12};
+        q.quicksort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
         
     }
 }
-
