f51c891be3f020344c05b887b0e6606d1f718bd9
sohailmadan <48715594+sohailmadan@users.noreply.github.com>
2020-09-19 11:14:40 +0530

    Update Binarytree.java

--- Binarytree.java
+++ Binarytree.java
@@ -1,21 +1,41 @@
 class Node{
     int key;
     Node left, right;
-    public Node(int data){
-        key = data;
-        left = right = null;
-    }
+    
 }
-public class Binarytree {
+public class Binarysearchtree {
     Node root;
-    public void traverse(Node node){
+    public void inordertraverse(Node node){
         if(node !=null){
-            traverse(node.left);
+         
+            inordertraverse(node.left);
             System.out.println("key "+ node.key);
-            traverse(node.right);
+            inordertraverse(node.right);
+            
+           
+            
         }
     }
     
+    public void preordertraverse(Node node){
+        if(node !=null){
+           
+            System.out.println("key "+ node.key);
+            preordertraverse(node.left);
+            preordertraverse(node.right);
+            
+            
+        }
+    }
+    public void postordertraverse(Node node){
+        if(node !=null){
+          
+            postordertraverse(node.left);
+            
+            postordertraverse(node.right);
+            System.out.println("key "+ node.key);            
+        }
+    }
     public static void main(String args[]) {
       
       Binarytree tree = new Binarytree();
@@ -26,7 +46,15 @@ public class Binarytree {
       tree.root.left.right= new Node(5);
       tree.root.right.left= new Node(6);
       tree.root.right.right= new Node(7);
-      tree.traverse(tree.root);
-      
+      System.out.println("IN order traverse begin");
+      tree.inordertraverse(tree.root);
+      System.out.println("IN order traverse end");
+      System.out.println("pre order traverse begin");
+      tree.preordertraverse(tree.root);
+      System.out.println("pre order traverse end");
+      System.out.println("post order traverse begin");
+      tree.postordertraverse(tree.root);
+      System.out.println("post order traverse end");
+
     }
 }
